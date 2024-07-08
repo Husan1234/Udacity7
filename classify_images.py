760e430de5233b22a49fa97dc68767c1fd37ce57
Husan1234 <157670760+Husan1234@users.noreply.github.com>
2024-07-08 18:54:07 -0400

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -3,7 +3,7 @@
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
 #                                                                             
 # PROGRAMMER: Husanpreet kaur
-# DATE CREATED:                                 
+# DATE CREATED:     JULY1                            
 # REVISED DATE: 
 # PURPOSE: Create a function classify_images that uses the classifier function 
 #          to create the classifier labels and then compares the classifier 
