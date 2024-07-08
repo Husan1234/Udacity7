d14b74a27e88e343d7466fdde3c2889b353fb101
Husan1234/ <sidhuhusan1234@gmail.com>
2024-07-08 18:41:03 -0400

    sidhuhusan1234@gmail.com Husan1234

---
+++ Udacity Project/Udacity Project.code-workspace
@@ -0,0 +1,13 @@
+{
+	"folders": [
+		{
+			"path": "."
+		},
+		{
+			"path": "../../OneDrive/Desktop/pet_images"
+		},
+		{
+			"path": "../../OneDrive/Desktop/uploaded_images"
+		}
+	]
+}
---
+++ print_results.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/print_results.py
+#                                                                             
+# PROGRAMMER: 
+# DATE CREATED:
+# REVISED DATE: 
+# PURPOSE: Create a function print_results that prints the results statistics
+#          from the results statistics dictionary (results_stats_dic). It 
+#          should also allow the user to be able to print out cases of misclassified
+#          dogs and cases of misclassified breeds of dog using the Results 
+#          dictionary (results_dic).  
+#         This function inputs:
+#            -The results dictionary as results_dic within print_results 
+#             function and results for the function call within main.
+#            -The results statistics dictionary as results_stats_dic within 
+#             print_results function and results_stats for the function call within main.
+#            -The CNN model architecture as model wihtin print_results function
+#             and in_arg.arch for the function call within main. 
+#            -Prints Incorrectly Classified Dogs as print_incorrect_dogs within
+#             print_results function and set as either boolean value True or 
+#             False in the function call within main (defaults to False)
+#            -Prints Incorrectly Classified Breeds as print_incorrect_breed within
+#             print_results function and set as either boolean value True or 
+#             False in the function call within main (defaults to False)
+#         This function does not output anything other than printing a summary
+#         of the final results.
+##
+# TODO 6: Define print_results function below, specifically replace the None
+#       below by the function definition of the print_results function. 
+#       Notice that this function doesn't to return anything because it  
+#       prints a summary of the results using results_dic and results_stats_dic
+# 
+def print_results(results_dic, results_stats_dic, model, 
+                  print_incorrect_dogs = False, print_incorrect_breed = False):
+    """
+    Prints summary results on the classification and then prints incorrectly 
+    classified dogs and incorrectly classified dog breeds if user indicates 
+    they want those printouts (use non-default values)
+    Parameters:
+      results_dic - Dictionary with key as image filename and value as a List 
+             (index)idx 0 = pet image label (string)
+                    idx 1 = classifier label (string)
+                    idx 2 = 1/0 (int)  where 1 = match between pet image and 
+                            classifer labels and 0 = no match between labels
+                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
+                            0 = pet Image 'is-NOT-a' dog. 
+                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
+                            'as-a' dog and 0 = Classifier classifies image  
+                            'as-NOT-a' dog.
+      results_stats_dic - Dictionary that contains the results statistics (either
+                   a  percentage or a count) where the key is the statistic's 
+                     name (starting with 'pct' for percentage or 'n' for count)
+                     and the value is the statistic's value 
+      model - Indicates which CNN model architecture will be used by the 
+              classifier function to classify the pet images,
+              values must be either: resnet alexnet vgg (string)
+      print_incorrect_dogs - True prints incorrectly classified dog images and 
+                             False doesn't print anything(default) (bool)  
+      print_incorrect_breed - True prints incorrectly classified dog breeds and 
+                              False doesn't print anything(default) (bool) 
+    Returns:
+           None - simply printing results.
+    """    
+    # Defining lists to populate dictionary
+filenames = ["Beagle_01141.jpg", "Beagle_01125.jpg", "skunk_029.jpg"]
+pet_labels = ["beagle", "beagle", "skunk"]
+classifier_labels = ["walker hound, walker foxhound", "beagle",
+                     "skunk, polecat, wood pussy"]
+pet_label_is_dog = [1, 1, 0]
+classifier_label_is_dog = [1, 1, 0]
+
+# Defining empty dictionary
+results_dic = dict()
+
+# Populates empty dictionary with both labels & indicates if they match (idx 2)
+for idx in range(len(filenames)):
+    # If first time key is assigned, initialize the list with pet & classifier labels
+    if filenames[idx] not in results_dic:
+        results_dic[filenames[idx]] = [pet_labels[idx], classifier_labels[idx]]
+
+    # Determine if pet_labels matches classifier_labels using in operator
+    # - so if pet label is 'in' classifier label it's a match
+    # ALSO since Key already exists because labels were added, append value to end of list for idx 2
+    # if pet image label was FOUND then there is a match 
+    if pet_labels[idx] in classifier_labels[idx]:
+        results_dic[filenames[idx]].append(1)
+    # if pet image label was NOT found then there is no match
+    else:
+        results_dic[filenames[idx]].append(0)
+
+# Populates dictionary with whether or not labels indicate a dog image (idx 3 & 4)
+for idx in range(len(filenames)):
+    # Key already exists, extend values to end of list for idx 3 & 4
+    results_dic[filenames[idx]].extend([pet_label_is_dog[idx], classifier_label_is_dog[idx]])
+
+# Iterates through the list to print the results for each filename
+for key in results_dic:
+    print("\nFilename=", key, "\npet_image Label=", results_dic[key][0],
+          "\nClassifier Label=", results_dic[key][1], "\nmatch=",
+          results_dic[key][2], "\nImage is dog=", results_dic[key][3],
+          "\nClassifier is dog=", results_dic[key][4])
+
+    # Provides classifications of the results
+    if sum(results_dic[key][2:]) == 3:
+        print("*Breed Match*")
+    if sum(results_dic[key][3:]) == 2:
+        print("*Is-a-Dog Match*")
+    if sum(results_dic[key][3:]) == 0 and results_dic[key][2] == 1:
+        print("*NOT-a-Dog Match*")
+      
